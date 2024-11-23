faa45885eac80a3055002ea62818baade0694853
Ilya <mnailJk@yandex.ru>
2024-11-23 17:21:02 +0300

    update core/exceptions

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -5,3 +5,18 @@ user_not_found_exception = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
     detail="User not found.",
 )
+
+user_alredy_exists_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="User with this email & full_name alredy exsists",
+)
+
+category_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Category not found.",
+)
+
+category_alredy_exists_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="Category with this name alredy exsists",
+)
